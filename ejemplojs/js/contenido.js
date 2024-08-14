c411563b27ea5c61c96aa8d36e49b3bb20a44c2e
Felipe Salamanca González <55007647+fsalamanca2000@users.noreply.github.com>
2024-08-14 17:46:06 -0500

    Add files via upload

---
+++ ejemplojs/js/contenido.js
@@ -0,0 +1,53 @@
+function nuevoTitulo() {
+    var contenido = document.getElementById('contenido');
+    var texto = document.getElementById("texto").value;
+    contenido.innerHTML = contenido.innerHTML + '<H1>' + texto + '</H1>';
+}
+function nuevoParrafo() {
+    var contenido = document.getElementById('contenido');
+    var texto = document.getElementById("texto").value;
+    contenido.innerHTML = contenido.innerHTML + '<p>' + texto + '</p>';
+}
+document.getElementById('imagen').onchange = function(evt) {
+    var entradaArchivo = evt.target,
+        archivos = entradaArchivo.files;
+    if (FileReader && archivos && archivos.length) {
+        var imagen = new FileReader();
+        imagen.onload = function() {
+            var etiquetaImg = document.createElement("img");
+            etiquetaImg.setAttribute('src', imagen.result);
+            etiquetaImg.setAttribute('width', '200px');
+            etiquetaImg.setAttribute('height', '180px');
+            var contenido = document.getElementById('contenido');
+            contenido.appendChild(etiquetaImg);
+        }
+        imagen.readAsDataURL(archivos[0]);
+    }
+}
+function imprimir(){
+    var divContents = document.getElementById("contenido").innerHTML;
+    var a = window.open('', '', 'height=500, width=500');
+    a.document.write('<html>');
+    a.document.write('<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />');
+    a.document.write('<body > <h1>Resultado</h1> <br>');
+    a.document.write(divContents);
+    a.document.write('</body></html>');
+    a.document.close();
+    a.print();
+    }
+    function nuevoEnlace(separador) {
+        var contenido = document.getElementById('contenido');
+        var texto = document.getElementById("texto").value;
+        var separador = ",";
+        var partes = texto.split(separador);
+        if (partes.length >= 2) {
+            var textoAntes = partes[0].trim();
+            var enlace = partes[1].trim(); 
+            var enlaceHTML = `<a href="${enlace}" target="_blank">${textoAntes}</a>`;
+            contenido.innerHTML += enlaceHTML;
+        } else {
+            contenido.innerHTML = 'Por favor, introduce el texto seguido de una coma y un enlace.';
+        }
+    }
+    
+    
